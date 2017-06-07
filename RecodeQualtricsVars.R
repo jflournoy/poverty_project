37f08f53ff403eae29912a6948fc555190c9b304
Rita.Lx <rludwig@uoregon.edu>
2017-06-07 13:49:24 -0700

    Added line to recode adj as Income, to match census var names

--- RecodeQualtricsVars.R
+++ RecodeQualtricsVars.R
@@ -3,5 +3,5 @@ data = read.table("/Users/rx/Documents/Research/Poverty/PhaseI_StudyOne/Data/Pov
 data$Sex=revalue(data$Gender,c("Male  "="1","Female"="2"))
 data$Education=revalue(data$Education, c("Less than high school"="1","High school graduate "="2","Some college         "="3","2 year degree        "="4","4 year degree        "="5","Professional degree  "="6","Masters              "="7"))
 data$Race=revalue(data$Ethnicity, c("White  not of Hispanic Origin    "="1","Black  not of Hispanic Origin    "="2","American Indian or Alaskan Native"="3","Asian or Pacific Islander        "="4","South Asian or Indian            "="4","Hispanic                         "="5","Other                            "="5"))
-
+data$Income=data$adj_inc
 write.csv(data, file="Poverty_PhaseI_StudyOne_Mturk_INR")
