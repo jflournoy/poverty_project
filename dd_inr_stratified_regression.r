bad20a1f156ed3c5896ac9671df60c067c31c18e
John Flournoy <uoflournoy@gmail.com>
2017-06-30 17:17:52 -0700

    simplified stratification scheme

--- dd_inr_stratified_regression.r
+++ dd_inr_stratified_regression.r
@@ -1,13 +1,26 @@
 library(survey)
 library(tidyverse)
 
-pums_xtabs <- readRDS('./pums_xtabs.RDS')
+pums_xtabs <- readRDS('~/code_new/poverty/pums_xtabs_no_race.RDS')
 
-INR <- read.table("./Poverty_PhaseI_StudyOne_Mturk_INR.csv", header=TRUE, sep=",")
-params <- read.table("./mCTB_params.csv", header=TRUE, sep=",")
-names(params)[names(params)=="subject_id"] <- "subject_ID"
-data <- left_join(INR, params, by="subject_ID")
+INR <- read.table("./Poverty_PhaseI_StudyOne_Mturk_INR", header=TRUE, sep=",")
+data <- INR
+# params <- read.table("./mCTB_params.csv", header=TRUE, sep=",")
+# names(params)[names(params)=="subject_id"] <- "subject_ID"
+# data <- left_join(INR, params, by="subject_ID")
 data$beedee <- data$b*data$d
+data$d10 <- scale(data$d)
+
+census_breaks <- c(-Inf, 22800, 43511, 72001, 117002, Inf)
+data$Income_raw <- data$Income
+data$Income <- cut(data$Income_raw,
+                   breaks = census_breaks, 
+                   dig.lab = -1)
+
+data$Education_raw <- data$Education
+data$Education <- as.numeric(c(`1`=1, `2`=2, `3`=3, `4`=4, `5`=4, `6`=4, `7`=4, `8`=4)[as.character(data$Education_raw)])
+
+xtabs(~Sex+Education+Income, data = data)
 
 #'
 #' # Planned regression
@@ -19,18 +32,38 @@ data$beedee <- data$b*data$d
 #' $$
 #' 
 
-dataSVY <- svydesign(ids = ~1, data = data)
+data_c <- within(data,{
+  INR = INR - 2.75
+  PScore = PScore - mean(PScore)
+  ConsciScore = ConsciScore- mean(ConsciScore)
+  BISscore = BISscore - mean(BISscore)
+})
+
+dataSVY <- svydesign(ids = ~1, data = data_c)
 dataPostStratSVY <- postStratify(design = dataSVY,
-                                 strata = ~Sex + Race + Education + Income,
-                                 population = pums_xtabs)
+                                 strata = ~Sex + Education+Income,
+                                 population = pums_xtabs,
+                                 partial = T)
 
 summary(weights(dataPostStratSVY)) #see blog post about trimming
-summary(dataPostStratSVY)
 
-summary(svyMod <- svyglm(d ~ (1 + INR + PScore + ConsciScore + BISscore), design = dataPostStratSVY))
-summary(glmMod <- glm(d ~ 1 + INR + PScore + ConsciScore + BISscore, data = data))
+summary(svyMod <- svyglm(d10 ~ (1 + INR), design = dataPostStratSVY))
+summary(svyMod <- svyglm(d10 ~ (1 + INR + PScore), design = dataPostStratSVY))
+summary(svyMod <- svyglm(d10 ~ (1 + INR + ConsciScore), design = dataPostStratSVY))
+summary(svyMod <- svyglm(d10 ~ (1 + INR + BISscore), design = dataPostStratSVY))
+summary(svyMod <- svyglm(d10 ~ (1 + INR + PScore + ConsciScore), design = dataPostStratSVY))
+summary(svyMod <- svyglm(d10 ~ (1 + INR + PScore + ConsciScore + BISscore), design = dataPostStratSVY))
+
+summary(glmMod <- glm(d10 ~ (1), data = data_c))
+summary(glmMod <- glm(d10 ~ (1 + INR), data = data_c))
+summary(glmMod <- glm(d10 ~ (1 + INR + PScore), data = data_c))
+summary(glmMod <- glm(d10 ~ (1 + INR + ConsciScore), data = data_c))
+summary(glmMod <- glm(d10 ~ (1 + INR + BISscore), data = data_c))
+summary(glmMod <- glm(d10 ~ (1 + INR + PScore + ConsciScore), data = data_c))
+summary(glmMod <- glm(d10 ~ (1 + INR + PScore + ConsciScore + BISscore), data = data_c))
+
 
 cat('Absolute deviation of coefficients:')
-round((coef(glmMod) - coef(svyMod)),2)
+round((coef(glmMod) - coef(svyMod)),4)
 cat('Proportional deviation of coefficients:')
 round((coef(glmMod) - coef(svyMod))/coef(svyMod),2)
--- read_census_data.r
+++ read_census_data.r
@@ -8,7 +8,7 @@ library(ggplot2)
 
 pus_a <- rbindlist(list(fread('~/code_new/poverty/census/ss15pusa.csv'),fread('~/code_new/poverty/census/ss15pusb.csv')))
 
-pus_shortened <- pus_a[,.(SERIALNO,SEX,AGEP,SCHL,RAC1P)]
+pus_shortened <- pus_a[,.(SERIALNO,SEX,AGEP,SCHL,RAC1P,PWGTP,ADJINC)]
 rm(pus_a);gc()
 
 convert_RAC1P <- function(RAC1P){
@@ -23,10 +23,40 @@ convert_RAC1P <- function(RAC1P){
                        ifelse(RAC1P %in% c(6,7), 4,
                               ifelse(RAC1P %in% c(8,9), 5, NA)))))
 }
+
+# SCHL
+# Educational attainment
+# bb .N/A (less than 3 years old)
+# 01 .No schooling completed
+# 02 .Nursery school, preschool
+# 03 .Kindergarten
+# 04 .Grade 1
+# 05 .Grade 2
+# 06 .Grade 3
+# 07 .Grade 4
+# 08 .Grade 5
+# 09 .Grade 6
+# 10 .Grade 7
+# 11 .Grade 8
+# 12 .Grade 9
+# 13 .Grade 10
+# 14 .Grade 11
+# 15 .12th grade - no diploma
+# 16 .Regular high school diploma
+# 17 .GED or alternative credential
+# 18 .Some college, but less than 1 year
+# 19 .1 or more years of college credit, no degree
+# 20 .Associate's degree
+# 21 .Bachelor's degree
+# 22 .Master's degree
+# 23 .Professional degree beyond a bachelor's degree
+# 24 .Doctorate degree
+
 convert_SCHL <- function(SCHL){
-  ifelse(SCHL %in% 1:15, 1,
-         ifelse(SCHL %in% 16:17, 2,
-                ifelse(SCHL %in% 18:19, 3, SCHL - 16)))
+  ifelse(SCHL %in% 1:15, 1, #Less than HS
+         ifelse(SCHL %in% 16:17, 2, #HS Only
+                ifelse(SCHL %in% 18:19, 3, #Some College
+                       ifelse(SCHL %in% 20:24, 4, NA)))) #Completed higher ed
 }
 convert_HINCP <- function(HINCP){
   ifelse(HINCP < 10000, 5000,
@@ -37,26 +67,43 @@ convert_HINCP <- function(HINCP){
 }
 
 hus_a <- rbindlist(list(fread('~/code_new/poverty/census/ss15husa.csv'),fread('~/code_new/poverty/census/ss15husb.csv')))
-hus_shortened <- hus_a[,.(SERIALNO,HINCP)]
+hus_shortened <- hus_a[,.(SERIALNO,HINCP,WGTP,ADJINC)]
 rm(hus_a);gc()
 
 setkey(pus_shortened, SERIALNO)
 setkey(hus_shortened, SERIALNO)
 
+#Matches expected estimates as reported by census:
+#SEX=1: 158,134,332
+#SEX=2: 163,284,489
+xtabs(PWGTP~SEX, data = pus_shortened)
+#Total population: 321,418,821
+sum(pus_shortened$PWGTP)
+
 pums_data <- hus_shortened[pus_shortened,]
 setnames(pums_data, 'SEX', 'Sex')
 pums_data[, Education := convert_SCHL(SCHL)]
 pums_data[, Race := convert_RAC1P(RAC1P)]
-pums_data[, Income := convert_HINCP(HINCP)]
+#Census quintiles, upper ranges:
+#22,800	43,511	72,001	117,002
+pums_data[, Income_adj := convert_HINCP(HINCP*(ADJINC*1e-6))]
+census_breaks <- c(-Inf, 22800, 43511, 72001, 117002, Inf)
+# acs_breaks <- c(-Inf, 22834, 43576, 70323, 112145, Inf)
+pums_data[, Income := cut(Income_adj, 
+                          breaks = census_breaks, 
+                          dig.lab = -1)]
 
-write.csv(pums_data, '~/code_new/poverty/pums_data_short.csv')
+#write.csv(pums_data, '~/code_new/poverty/pums_data_short.csv')
 
-pums_xtabs <- xtabs(~Sex + Race + Education + Income, pums_data)
+pums_xtabs <- xtabs(PWGTP~Sex + Race + Education + Income, pums_data)
 saveRDS(pums_xtabs, '~/code_new/poverty/pums_xtabs.RDS')
 
-pums_xtabs_no_inc <- xtabs(~Sex + Race + Education, pums_data)
+pums_xtabs_no_race <- xtabs(PWGTP~Sex + Education + Income, pums_data)
+saveRDS(pums_xtabs_no_race, '~/code_new/poverty/pums_xtabs_no_race.RDS')
+
+pums_xtabs_no_inc <- xtabs(PWGTP~Sex + Race + Education, pums_data)
 saveRDS(pums_xtabs_no_inc, '~/code_new/poverty/pums_xtabs_no_inc.RDS')
 
-pums_xtabs_no_inc_no_ed <- xtabs(~Sex + Race, pums_data)
+pums_xtabs_no_inc_no_ed <- xtabs(PWGTP~Sex + Race, pums_data)
 saveRDS(pums_xtabs_no_inc_no_ed, '~/code_new/poverty/pums_xtabs_no_inc_no_ed.RDS')
 
